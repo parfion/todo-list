8042b106360e13358afa0e6fd6d882c7dd91f6c2
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-08-23 16:26:56 +0300

    add styles

--- index.js
+++ index.js
@@ -1,53 +1,30 @@
-const addInput = document.querySelector('.add__input');
 const addButton = document.querySelector('.add__button');
 const container = document.querySelector('.container');
-// const template = document.querySelector('.template');
 const addForm = document.querySelector('.add');
+let taskText;
 
+function disabledButton() {
+    addButton.disabled = true;
+    addButton.classList.add('add__button-inactive');
+};
 
-
-
-const template = document.querySelector('.template')
-const templateContent = template.content;
-const templateItem = templateContent.querySelector('.template__content');
-
-function createToDo(data) {
-    const newTask = templateItem.cloneNode(true);
-    const nameTask = newTask.querySelector('.template__text');
-    nameTask.textContent = data;
-
-    // слушатели
-    const deleteButton = newTask.querySelector('.template__button-delete');
-    deleteButton.addEventListener('click', () => container.removeChild(newTask));
-
-    const doneButton = newTask.querySelector('.template__button-done');
-    doneButton.addEventListener('click', () => nameTask.classList.add('done'));
-
-    return newTask;
+function enabledButton() {
+    addButton.disabled = false; 
+    addButton.classList.remove('add__button-inactive');
 }
 
-addForm.addEventListener('submit', (e) => {
+function enableAdd() {
+    taskText = document.querySelector('.add__input').value;
+    (taskText === '') ? disabledButton() : enabledButton();
+}
+
+addButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const inputValue = document.querySelector('.add__input').value
-    console.log(inputValue);
-    const newToDo = createToDo(inputValue);
-    container.append(newToDo);
+
+    taskText = document.querySelector('.add__input').value;
+    const newTask = new Task(taskText);
+    const newElement = newTask.generateTask();
+    container.append(newElement);
     addForm.reset();
+    disabledButton();
 })
-
-// function cloneTemplate() {
-//     const templateInput = template.content.cloneNode(true);
-//     return templateInput;
-// };
-
-// function addTask() {
-//     console.log('aaa')
-//     const task = cloneTemplate();
-//     container.append(task);
-//     const taskText = document.querySelector('.template__text');
-//     console.log(taskText);
-//     taskText.textContent = addInput.value;
-// }
-
-// addForm.addEventListener('submit', (e) => {e.preventDefault(); addTask()})
-
